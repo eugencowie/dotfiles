441c0f8c0160cfd849d1d4023c3078aec0707fe5
Eugén Cowie <eugencowie@users.noreply.github.com>
2026-03-20 19:37:22 +0000

    Enable flake support

--- hosts/hp-250-g9/configuration.nix
+++ hosts/hp-250-g9/configuration.nix
@@ -17,6 +17,9 @@
   programs.zsh.enable = true;
   users.users.nixos.shell = pkgs.zsh;
 
+  # Enable flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
