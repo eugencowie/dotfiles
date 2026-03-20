9d7b59e6cfb35701bd28455830027c5722339251
Eugén Cowie <eugencowie@users.noreply.github.com>
2026-03-20 19:37:22 +0000

    Clean up configuration

--- hosts/hp-250-g9/configuration.nix
+++ hosts/hp-250-g9/configuration.nix
@@ -1,16 +1,9 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-# NixOS-WSL specific options are documented on the NixOS-WSL repository:
-# https://github.com/nix-community/NixOS-WSL
-
-{ config, lib, pkgs, ... }:
-
-{
+{ config, lib, pkgs, ... }: {
+ 
+  # Enable support for running NixOS as a WSL distribution
   wsl.enable = true;
   wsl.defaultUser = "nixos";
-
+ 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
