f1341ece760c40deac49d28411cb9d73f90c3d1f
Eugén Cowie <eugencowie@users.noreply.github.com>
2026-03-22 19:16:13 +0000

    Create hardware configuration

--- hosts/macbook-air-m1/configuration.nix
+++ hosts/macbook-air-m1/configuration.nix
@@ -1,13 +1,16 @@
 { self, config, lib, pkgs, ... }: {
 
+  # Include the results of the hardware scan
+  imports = [ ./hardware-configuration.nix ];
+
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages =
     [ pkgs.vim
     ];
 
-  # Necessary for using flakes on this system.
-  nix.settings.experimental-features = "nix-command flakes";
+  # Enable flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Enable alternative shell support in nix-darwin.
   # programs.fish.enable = true;
@@ -17,9 +20,6 @@
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
-  system.stateVersion = 6;
-
-  # The platform the configuration will be used on.
-  nixpkgs.hostPlatform = "aarch64-darwin";
+  system.stateVersion = 6; # Did you read the comment?
 
 }
