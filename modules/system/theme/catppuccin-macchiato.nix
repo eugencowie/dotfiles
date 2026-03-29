622d0b30b9de01215e171ee4671e4ebce36057f1
Eugén Cowie <eugencowie@users.noreply.github.com>
2026-03-29 03:31:41 +0100

    Fix font configuration

--- modules/system/theme/catppuccin-macchiato.nix
+++ modules/system/theme/catppuccin-macchiato.nix
@@ -5,6 +5,12 @@
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
     polarity = "dark";
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.iosevka-term;
+        name = "IosevkaTerm Nerd Font";
+      };
+    };
   };
 
 }
