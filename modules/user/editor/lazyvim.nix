9b9ba7b222c6b2dae28345b379b184df3b0695be
Eugén Cowie <eugencowie@users.noreply.github.com>
2026-03-30 01:50:58 +0100

    Enable Copilot plugin for LazyVim

--- modules/user/editor/lazyvim.nix
+++ modules/user/editor/lazyvim.nix
@@ -5,7 +5,12 @@
   ];
 
   # Enable LazyVim
-  programs.lazyvim.enable = true;
+  programs.lazyvim = {
+    enable = true;
+    extras.ai.copilot.enable = true;
+  };
+
+  # Set Neovim as the default editor
   programs.neovim.defaultEditor = true;
 
 }
