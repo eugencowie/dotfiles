184c40d852ea158007ae8b0ee850577835d0298f
Eugén Cowie <eugencowie@users.noreply.github.com>
2026-03-31 15:29:47 +0100

    Restore VS Code server configuration

--- hosts/hp-250-g9/configuration.nix
+++ hosts/hp-250-g9/configuration.nix
@@ -28,6 +28,9 @@
   my.user.name = "nixos";
   my.user.config = import ./home.nix;
 
+  # Required for VS Code Server
+  programs.nix-ld.enable = true;
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
