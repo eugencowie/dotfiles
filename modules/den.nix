724b5c1bd5280c384ec385c59f7a706024a00113
Eugén Cowie <eugencowie@users.noreply.github.com>
2026-04-03 22:18:48 +0100

    Move home state version into den module

--- modules/den.nix
+++ modules/den.nix
@@ -43,4 +43,14 @@
   den.default.nixos.system.stateVersion = "25.11";
   den.default.darwin.system.stateVersion = 6;
 
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  den.default.homeManager.home.stateVersion = "25.11";
+
 }
