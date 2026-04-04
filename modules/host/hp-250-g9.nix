36c038ae73496f42a1e4662387bf72d1bda08b1c
Eugén Cowie <eugencowie@users.noreply.github.com>
2026-04-04 12:11:39 +0100

    Move host configuration into host aspects

--- modules/host/hp-250-g9.nix
+++ modules/host/hp-250-g9.nix
@@ -13,7 +13,28 @@
     ];
 
     # Import legacy configuration
-    os.imports = [ ../../hosts/hp-250-g9/configuration.nix ];
+    os.imports = [
+
+      # Include the results of the hardware scan
+      ../../hosts/hp-250-g9/hardware-configuration.nix
+      
+      # Include custom option definitions
+      ../../options/user.nix
+
+      # Basic system configuration
+      ../../legacy/system/time/europe/london.nix
+      ../../legacy/system/locale/english/british.nix
+
+      # Customise login environment
+      ../../legacy/system/shell/zsh.nix
+      ../../legacy/system/home/homeManager.nix
+      ../../legacy/system/nix/flakes.nix
+
+    ];
+
+    # Define user configuration
+    os.my.user.name = "nixos";
+    os.my.user.config = import ../../hosts/hp-250-g9/home.nix;
 
     # Enable support for running NixOS as a WSL distribution
     wsl.enable = true;
--- modules/host/macbook-air-m1.nix
+++ modules/host/macbook-air-m1.nix
@@ -10,7 +10,24 @@
     ];
 
     # Import legacy configuration
-    os.imports = [ ../../hosts/macbook-air-m1/configuration.nix ];
+    os.imports = [
+
+      # Include the results of the hardware scan
+      ../../hosts/macbook-air-m1/hardware-configuration.nix
+
+      # Include custom option definitions
+      ../../options/user.nix
+
+      # Customise login environment
+      ../../legacy/system/shell/zsh.nix
+      ../../legacy/system/home/homeManager.nix
+      ../../legacy/system/nix/flakes.nix
+
+    ];
+
+    # Define user configuration
+    os.my.user.name = "eugen";
+    os.my.user.config = import ../../hosts/macbook-air-m1/home.nix;
 
   };
 
