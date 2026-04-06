96f651547c50e0b43d7f11e8a904a6a0bf1a0bc9
Eugén Cowie <eugencowie@users.noreply.github.com>
2026-04-07 00:17:52 +0100

    Move zen input

--- modules/flake-file.nix
+++ modules/flake-file.nix
@@ -1,16 +1,7 @@
 { inputs, lib, ... }: {
 
-  flake-file.inputs = {
-
-    # Generate flake.nix from module options
-    flake-file.url = "github:vic/flake-file";
-
-    # Module for managing Zen Browser in Home Manager
-    zen-browser.url = "github:0xc000022070/zen-browser-flake/beta";
-    zen-browser.inputs.nixpkgs.follows = "nixpkgs";
-    zen-browser.inputs.home-manager.follows = "home-manager";
-
-  };
+  # Generate flake.nix from module options
+  flake-file.inputs.flake-file.url = "github:vic/flake-file";
 
   # Use flake-file for dendritic modules
   imports = [ (inputs.flake-file.flakeModules.dendritic or { }) ];
