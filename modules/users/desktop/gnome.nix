bf93072d4232bbd238335173b68561c1f6fe3e46
Eugén Cowie <eugencowie@users.noreply.github.com>
2026-04-08 22:19:01 +0100

    Remove legacy options

--- modules/users/desktop/gnome.nix
+++ modules/users/desktop/gnome.nix
@@ -2,18 +2,6 @@
 
   den.aspects.desktop.provides.gnome.homeManager = {
 
-    # Configure GNOME
-    dconf = {
-      enable = true;
-      settings = {
-        "org/gnome/mutter" = {
-          experimental-features = [
-            "scale-monitor-framebuffer" # fractional scaling
-          ];
-        };
-      };
-    };
-
     # Disable KDE Stylix target on GNOME host
     stylix.targets.kde.enable = false;
 
