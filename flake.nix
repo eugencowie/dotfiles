07abc1566130a92f0122c28d3254c9c2faadaede
Eugén Cowie <eugencowie@users.noreply.github.com>
2026-04-29 19:55:49 +0100

    Update flake files

--- flake.nix
+++ flake.nix
@@ -29,7 +29,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-lib.follows = "nixpkgs";
     stylix = {
       url = "github:nix-community/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
